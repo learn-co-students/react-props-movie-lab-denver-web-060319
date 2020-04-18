35e086c9167de9da49b6c8cf231d249ff82c4152
Blake A. Long <blake.a.long@gmail.com>
2020-04-17 20:02:32 -0600

    Done.

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -24,9 +24,9 @@ export default class CardBack extends Component {
       <div className="card-back">
         <h3 className="title">{this.props.title}</h3>
         <span />
-        {this.generateRatingElement()}
+          {this.generateRatingElement()}
         <span />
-    <h5 className="genres">{this.props.genres.join(', ')}</h5>
+        <h5 className="genres">{this.props.genres.join(', ')}</h5>
       </div>
     )
   }
